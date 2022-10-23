505a9375b0fffd9eaeba706edf0090c5ec3032a3
Milo-Python <79796228+Milo-Python@users.noreply.github.com>
2022-10-23 15:24:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ import os
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host="0.0.0.0", threaded=True, port=int(os.environ.get('PORT', 33507)))
+    # app.run(host="0.0.0.0", threaded=True, port=int(os.environ.get('PORT', 33507)))
+    app.run(host="0.0.0.0", threaded=True, port=8080)

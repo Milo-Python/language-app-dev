48ad2451885db6ee59608bab87b7177c2bf7e1de
Kasia <metok@vp.pl>
2023-02-12 15:22:33 +0100

    port change

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = create_app()
 if __name__ == '__main__':
     # app = create_app()
     # app.run(host="0.0.0.0", threaded=True, port=int(os.environ.get('PORT', 33507)))
-    app.run(host="0.0.0.0", threaded=True, port=8080)
+    app.run(host="0.0.0.0", threaded=True, port=8000)
